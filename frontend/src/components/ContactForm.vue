6e74e6c1a739cc041b3bdf81b08e30b78cc4ecf5
bee5325 <yb.bee5325@gmail.com>
2022-03-03 18:07:46 +0800

    Fixed contact form on mobile

--- frontend/src/components/ContactForm.vue
+++ frontend/src/components/ContactForm.vue
@@ -15,9 +15,9 @@ async function submit() {
 <template>
   <form @submit.prevent="submit">
     <label class="block font-bold">Feedback</label>
-    <textarea class="w-2xl h-200px border-gray-300 border-1 border-solid px-2 py-1" v-model="feedback"></textarea>
+    <textarea class="w-full md:w-xl md:mx-0 h-200px border-gray-300 border-1 border-solid px-2 py-1" v-model="feedback"></textarea>
     <label class="block font-bold">Your email</label>
-    <input class="w-2xl border-gray-300 border-1 border-solid px-2 py-1 block" type="email" v-model="email" />
+    <input class="w-full md:w-xl md:mx-0 border-gray-300 border-1 border-solid px-2 py-1 block" type="email" v-model="email" />
     <button class="btn ml-0 mt-5" type="submit" :disabled="submited">Submit</button>
     <p v-if="submited" class="inline-block ml-3 text-green-700">Thanks for your feedback.</p>
   </form>
